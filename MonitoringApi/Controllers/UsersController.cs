3724d168a5bf7168d45a1794ce6472d15410e7e1
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-05-24 12:59:49 +0200

    Add project files.

---
+++ MonitoringApi/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitoringApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    ILogger<UsersController> _logger;
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        if (id < 0 || id > 100)
+        {
+            _logger.LogWarning("The given Id if {Id} is invalid", id);
+            return BadRequest("The index was out of range.");
+        }
+
+        _logger.LogInformation(@"The api\Users\{id} was called", id);
+        return Ok($"Value: {id}");
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
---
+++ MonitoringApi/Program.cs
@@ -0,0 +1,25 @@
+using MonitoringApi.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site health check")
+    .AddCheck<RandomHealthCheck>("Database health check");
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHealthChecks("/health");
+
+app.Run();
